35099ec98d3d9dc154e24b14f21376d54708b226
Thiago Elias <thiagoelias99@gmail.com>
2023-07-06 14:21:17 -0300

    feat: added es-lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,35 @@
 import { useFetch } from "./hooks/useFetch";
 import { IIngredient } from "./models/IIngredient";
-import { WebClient as webClient } from "./services/webclient/axiosConfig";
-import { useEffect } from "react";
 import { WebClient as client } from "./services/webclient/axiosConfig";
 import { AxiosError } from "axios";
 
 function App() {
-  const route = "ingredients"
+    const route = "ingredients";
 
-  // useEffect(() => {
-  // webClient().get("ingredients").then(response => console.log)
-  // }, []);
+    const { data } = useFetch<IIngredient[]>(route);
 
-  const { data, isLoading, error } = useFetch<IIngredient[]>(route)
+    data?.forEach(ingredient => console.log(ingredient.description));
 
-  data?.forEach(ingredient => console.log(ingredient.description))
+    const postNow = true;
 
-  const postNow = true;
+    if (postNow) {
+        client().post(route, {
+            description: "Outro queijo"
+        })
+            .catch(err => {
+                if (err instanceof AxiosError) {
+                    console.log(err.response?.status);
+                } else (
+                    console.log(err)
+                );
+            });
+    }
 
-  if (postNow) {
-    client().post(route, {
-      description: "Outro queijo"
-    })
-      .catch(err => {
-        if (err instanceof AxiosError) {
-          console.log(err.response?.status)
-        } else (
-          console.log(err)
-        )
-      })
-  }
-
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>App</h1>
+        </div>
+    );
 }
 
 export default App;
